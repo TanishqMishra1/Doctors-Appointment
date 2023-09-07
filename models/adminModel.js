0c71fcfbafa6e270a097a8f774e51e027a552b93
Tanishq <tanishqmishra3152000@gmail.com>
2023-09-07 19:02:50 +0530

    Adding Code

---
+++ models/adminModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const adminSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isDoctor: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: true,
+    },
+    seenNotifications: {
+      type: Array,
+      default: [],
+    },
+    unseenNotifications: {
+      type: Array,
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const adminModel = mongoose.model("admins", adminSchema);
+
+module.exports = adminModel;
